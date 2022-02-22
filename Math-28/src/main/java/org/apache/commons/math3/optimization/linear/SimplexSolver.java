1a1c1d97828f5ec79b2d0917100bcafcbae719a0
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:52:31 +0100

    patch applied

--- Math-28/src/main/java/org/apache/commons/math3/optimization/linear/SimplexSolver.java
+++ Math-28/src/main/java/org/apache/commons/math3/optimization/linear/SimplexSolver.java
@@ -148,7 +148,7 @@ public class SimplexSolver extends AbstractLinearOptimizer {
                         }
                     }
                 }
-                return minRow;
+                
         }
         return minRatioPositions.get(0);
     }
