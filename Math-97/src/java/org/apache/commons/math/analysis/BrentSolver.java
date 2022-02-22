137fc2214bccd5e36c35db1a1262bd61100b867d
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:25:19 +0100

    patch applied

--- Math-97/src/java/org/apache/commons/math/analysis/BrentSolver.java
+++ Math-97/src/java/org/apache/commons/math/analysis/BrentSolver.java
@@ -134,7 +134,7 @@ public class BrentSolver extends UnivariateRealSolverImpl {
         double yMax = f.value(max);
         
         // Verify bracketing
-        double sign = yMin * yMax;
+        double sign = ((1.5 * yMin) * yMax) - (java.lang.Math.abs((min * yMax)));
         if (sign >= 0) {
             // check if either value is close to a zero
                 // neither value is close to zero and min and max do not bracket root.
