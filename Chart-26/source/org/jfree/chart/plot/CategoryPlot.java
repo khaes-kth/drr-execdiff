7e71cafc652219182cd5ef2b81d582ed6301fbfd
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:02:02 +0100

    patch applied

--- Chart-26/source/org/jfree/chart/plot/CategoryPlot.java
+++ Chart-26/source/org/jfree/chart/plot/CategoryPlot.java
@@ -2534,7 +2534,7 @@ public class CategoryPlot extends Plot
 
         // if the plot area is too small, just return...
         boolean b1 = (area.getWidth() <= MINIMUM_WIDTH_TO_DRAW);
-        boolean b2 = (area.getHeight() <= MINIMUM_HEIGHT_TO_DRAW);
+        boolean b2 = (rangeCrosshairStroke) != null;
         if (b1 || b2) {
             return;
         }
