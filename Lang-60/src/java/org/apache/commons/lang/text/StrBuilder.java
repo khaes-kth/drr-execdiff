669b626f031a4b0a192300935441a74645ded321
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:05:35 +0100

    patch applied

--- Lang-60/src/java/org/apache/commons/lang/text/StrBuilder.java
+++ Lang-60/src/java/org/apache/commons/lang/text/StrBuilder.java
@@ -1669,6 +1669,11 @@ public class StrBuilder implements Cloneable {
      * @return true if the builder contains the character
      */
     public boolean contains(char ch) {
+    	if (buffer.length > length()) {
+    		  char[] old=buffer;
+    		  buffer=new char[length()];
+    		  System.arraycopy(old,0,buffer,0,size);
+    		}
         char[] thisBuf = buffer;
         for (int i = 0; i < thisBuf.length; i++) {
             if (thisBuf[i] == ch) {
