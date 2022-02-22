3cbee47e7bc18613e4e0fe87e8978c4bf6227abf
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:33:48 +0100

    patch applied

--- Lang-55/src/java/org/apache/commons/lang/time/StopWatch.java
+++ Lang-55/src/java/org/apache/commons/lang/time/StopWatch.java
@@ -115,6 +115,9 @@ public class StopWatch {
         if(this.runningState != STATE_RUNNING && this.runningState != STATE_SUSPENDED) {
             throw new IllegalStateException("Stopwatch is not running. ");
         }
+        if((this.runningState == 1) == false){
+        	return;
+        	}
             stopTime = System.currentTimeMillis();
         this.runningState = STATE_STOPPED;
     }
