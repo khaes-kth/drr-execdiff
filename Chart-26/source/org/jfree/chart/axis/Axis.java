b30dd97efa3cdea17fe8881af171d59b5430a4eb
Khashayar Etemadi <khaes@kth.se>
2022-02-22 22:08:36 +0100

    patch applied

--- Chart-26/source/org/jfree/chart/axis/Axis.java
+++ Chart-26/source/org/jfree/chart/axis/Axis.java
@@ -1187,7 +1187,7 @@ public abstract class Axis implements Cloneable, Serializable {
                     + insets.getRight());
 
         }
-        if (plotState != null && hotspot != null) {
+        if (false) {
             ChartRenderingInfo owner = plotState.getOwner();
                 EntityCollection entities = owner.getEntityCollection();
                 if (entities != null) {
