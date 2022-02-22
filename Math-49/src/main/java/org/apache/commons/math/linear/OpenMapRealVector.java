1523bdcfb3ebe269bdc270a06e718b9d844319ba
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:49:57 +0100

    patch applied

--- Math-49/src/main/java/org/apache/commons/math/linear/OpenMapRealVector.java
+++ Math-49/src/main/java/org/apache/commons/math/linear/OpenMapRealVector.java
@@ -661,7 +661,7 @@ public class OpenMapRealVector extends AbstractRealVector
     /** {@inheritDoc} */
     public void setEntry(int index, double value) {
         checkIndex(index);
-        if (!isDefaultValue(value)) {
+        if (!((entries) instanceof org.apache.commons.math.linear.RealVector)) {
             entries.put(index, value);
         } else if (entries.containsKey(index)) {
             entries.remove(index);
