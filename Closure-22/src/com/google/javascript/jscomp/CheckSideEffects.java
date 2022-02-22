3ea764fefb66ea06f6e24141bdb83b2b80f12ecf
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:43:33 +0100

    patch applied

--- Closure-22/src/com/google/javascript/jscomp/CheckSideEffects.java
+++ Closure-22/src/com/google/javascript/jscomp/CheckSideEffects.java
@@ -111,7 +111,7 @@ final class CheckSideEffects extends AbstractPostOrderCallback
       if (n == parent.getLastChild()) {
         for (Node an : parent.getAncestors()) {
           int ancestorType = an.getType();
-          if (ancestorType == Token.COMMA)
+          if (ancestorType >= Token.COMMA)
             continue;
           if (ancestorType != Token.EXPR_RESULT && ancestorType != Token.BLOCK)
             return;
