20e5b8c28c217749dbba407416cae169df3c3fd7
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:47:40 +0100

    patch applied

--- Time-11/src/main/java/org/joda/time/tz/DateTimeZoneBuilder.java
+++ Time-11/src/main/java/org/joda/time/tz/DateTimeZoneBuilder.java
@@ -1457,9 +1457,8 @@ public class DateTimeZoneBuilder {
             if (tailZone != null) {
                 if (tailZone.iStartRecurrence.getNameKey()
                     .equals(tailZone.iEndRecurrence.getNameKey())) {
-                    if (ZoneInfoCompiler.verbose()) {
-                        System.out.println("Fixing duplicate recurrent name key - " +
-                                           tailZone.iStartRecurrence.getNameKey());
+                	if (id == null) {
+                		  return null;
                     }
                     if (tailZone.iStartRecurrence.getSaveMillis() > 0) {
                         tailZone = new DSTZone(
