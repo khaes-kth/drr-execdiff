8d96d65acf12f1a96766ca7f740cba7807e3beaf
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:37:32 +0100

    patch applied

--- Closure-123/src/com/google/javascript/jscomp/CodeGenerator.java
+++ Closure-123/src/com/google/javascript/jscomp/CodeGenerator.java
@@ -282,7 +282,7 @@ class CodeGenerator {
       case Token.HOOK: {
         Preconditions.checkState(childCount == 3);
         int p = NodeUtil.precedence(type);
-        Context rhsContext = Context.OTHER;
+        CodeGenerator.Context rhsContext=CodeGenerator.Context.IN_FOR_INIT_CLAUSE;
         addExpr(first, p + 1, context);
         cc.addOp("?", true);
         addExpr(first.getNext(), 1, rhsContext);
