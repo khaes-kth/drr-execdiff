e2ae35fefa69922ef8b4c467d764f16a42a1744c
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:58:29 +0100

    patch applied

--- Lang-7/src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ Lang-7/src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -450,7 +450,6 @@ public class NumberUtils {
             throw new NumberFormatException("A blank string is not a valid number");
         }  
         if (str.startsWith("--")) {
-            return null;
         }
         if (str.startsWith("0x") || str.startsWith("-0x") || str.startsWith("0X") || str.startsWith("-0X")) {
             int hexDigits = str.length() - 2; // drop 0x
