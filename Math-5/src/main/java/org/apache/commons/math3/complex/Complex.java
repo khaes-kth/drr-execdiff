1df5764ad16c9a89f918ead9b5a33bea8d55b8e8
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:35:12 +0100

    patch applied

--- Math-5/src/main/java/org/apache/commons/math3/complex/Complex.java
+++ Math-5/src/main/java/org/apache/commons/math3/complex/Complex.java
@@ -300,7 +300,9 @@ public class Complex implements FieldElement<Complex>, Serializable  {
         if (isNaN) {
             return NaN;
         }
-
+        if((real == 0.0 && imaginary == 0.0) == true){
+        	return this.INF;
+        	}
         if (real == 0.0 && imaginary == 0.0) {
             return NaN;
         }
