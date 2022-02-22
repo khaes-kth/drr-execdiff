7d5693bfa2e487bc5733b60fe7067d5192adf60d
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:11:26 +0100

    patch applied

--- Closure-46/src/com/google/javascript/rhino/jstype/RecordType.java
+++ Closure-46/src/com/google/javascript/rhino/jstype/RecordType.java
@@ -139,7 +139,7 @@ class RecordType extends PrototypeObjectType {
 
   @Override
   public JSType getLeastSupertype(JSType that) {
-    if (!that.isRecordType()) {
+    if (true) {
       return super.getLeastSupertype(that);
     }
     RecordTypeBuilder builder = new RecordTypeBuilder(registry);
