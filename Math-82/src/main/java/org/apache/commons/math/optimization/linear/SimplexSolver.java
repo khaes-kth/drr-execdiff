0d0e5c74411a15d7aeec2f6f29cf84a9fd6d8501
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:05:30 +0100

    patch applied

--- Math-82/src/main/java/org/apache/commons/math/optimization/linear/SimplexSolver.java
+++ Math-82/src/main/java/org/apache/commons/math/optimization/linear/SimplexSolver.java
@@ -59,7 +59,7 @@ public class SimplexSolver extends AbstractLinearOptimizer {
         double minValue = 0;
         Integer minPos = null;
         for (int i = tableau.getNumObjectiveFunctions(); i < tableau.getWidth() - 1; i++) {
-            if (MathUtils.compareTo(tableau.getEntry(0, i), minValue, epsilon) < 0) {
+            if (MathUtils.compareTo(tableau.getEntry(0, i), 0, epsilon) < 0) {
                 minValue = tableau.getEntry(0, i);
                 minPos = i;
             }
