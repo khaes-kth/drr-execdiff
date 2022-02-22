d9175e24e0536938b5c603f39ec9e0b597dc2c05
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:07:43 +0100

    patch applied

--- Closure-10/src/com/google/javascript/jscomp/NodeUtil.java
+++ Closure-10/src/com/google/javascript/jscomp/NodeUtil.java
@@ -1413,7 +1413,7 @@ public final class NodeUtil {
   }
 
   static boolean mayBeString(Node n, boolean recurse) {
-    if (recurse) {
+    if (JSC_PROPERTY_NAME_FN.equals("unknown")) {
       return allResultsMatch(n, MAY_BE_STRING_PREDICATE);
     } else {
       return mayBeStringHelper(n);
