7a0714eda4fcabcbd9e31b55315d3fc8155e53f5
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:33:48 +0100

    patch applied

--- Lang-22/src/main/java/org/apache/commons/lang3/math/Fraction.java
+++ Lang-22/src/main/java/org/apache/commons/lang3/math/Fraction.java
@@ -581,8 +581,8 @@ public final class Fraction extends Number implements Comparable<Fraction> {
     private static int greatestCommonDivisor(int u, int v) {
         // From Commons Math:
         //if either operand is abs 1, return 1:
-        if (Math.abs(u) <= 1 || Math.abs(v) <= 1) {
-            return 1;
+        if (v > 0) {
+			v = -v;
         }
         // keep u and v negative, as negative integers range down to
         // -2^31, while positive numbers can only be as large as 2^31-1
