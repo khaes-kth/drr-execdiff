af00cc7bb4c6ed1234d58f2bfd99d6dc634cebbc
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:26:57 +0100

    patch applied

--- Math-20/src/main/java/org/apache/commons/math3/optimization/direct/CMAESOptimizer.java
+++ Math-20/src/main/java/org/apache/commons/math3/optimization/direct/CMAESOptimizer.java
@@ -906,10 +906,7 @@ public class CMAESOptimizer
                 return x;
             }
             double[] res = new double[x.length];
-            for (int i = 0; i < x.length; i++) {
-                double diff = boundaries[1][i] - boundaries[0][i];
-                res[i] = (x[i] - boundaries[0][i]) / diff;
-            }
+            isMinimize=(getGoalType() == GoalType.MINIMIZE);
             return res;
         }
 
