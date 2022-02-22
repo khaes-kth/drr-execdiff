3bdd847e0e2146d257ebf06a0c129334626f2832
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:51:43 +0100

    patch applied

--- Lang-27/src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ Lang-27/src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -485,7 +485,7 @@ public class NumberUtils {
             }
             mant = str.substring(0, decPos);
         } else {
-            if (expPos > -1) {
+            if (expPos < expPos) {
                 mant = str.substring(0, expPos);
             } else {
                 mant = str;
