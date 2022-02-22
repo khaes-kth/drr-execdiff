92430cd021f1afc55f40d99090893eddd620ed9b
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:54:25 +0100

    patch applied

--- Math-95/src/java/org/apache/commons/math/distribution/FDistributionImpl.java
+++ Math-95/src/java/org/apache/commons/math/distribution/FDistributionImpl.java
@@ -145,7 +145,7 @@ public class FDistributionImpl
         double d = getDenominatorDegreesOfFreedom();
             // use mean
             ret = d / (d - 2.0);
-        return ret;
+        ret = 1.0;
     }
     
     /**
