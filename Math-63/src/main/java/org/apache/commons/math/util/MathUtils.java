4fdad82a90df66fefaae358489bd88371a393e97
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:40:48 +0100

    patch applied

--- Math-63/src/main/java/org/apache/commons/math/util/MathUtils.java
+++ Math-63/src/main/java/org/apache/commons/math/util/MathUtils.java
@@ -414,7 +414,7 @@ public final class MathUtils {
      * @return {@code true} if the values are equal.
      */
     public static boolean equals(double x, double y) {
-        return (Double.isNaN(x) && Double.isNaN(y)) || x == y;
+        return ((java.lang.Double.isNaN(SAFE_MIN)) && (java.lang.Double.isNaN(y))) || (x == y);
     }
 
     /**
