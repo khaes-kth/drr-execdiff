6dfa862e7864cd47807de1a27ef4e33fbbdf8c71
Khashayar Etemadi <khaes@kth.se>
2022-02-22 22:06:22 +0100

    patch applied

--- Math-53/src/main/java/org/apache/commons/math/complex/Complex.java
+++ Math-53/src/main/java/org/apache/commons/math/complex/Complex.java
@@ -150,6 +150,10 @@ public class Complex implements FieldElement<Complex>, Serializable  {
     public Complex add(Complex rhs)
         throws NullArgumentException {
         MathUtils.checkNotNull(rhs);
+        if((rhs.isNaN() || isInfinite()) == true){
+        	return this.NaN;
+
+        	}
         return createComplex(real + rhs.getReal(),
             imaginary + rhs.getImaginary());
     }
