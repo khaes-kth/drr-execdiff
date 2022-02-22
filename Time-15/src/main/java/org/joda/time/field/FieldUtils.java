c24932ee3f562f66f282ad80d5d9d3a0504917c6
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:44:20 +0100

    patch applied

--- Time-15/src/main/java/org/joda/time/field/FieldUtils.java
+++ Time-15/src/main/java/org/joda/time/field/FieldUtils.java
@@ -135,6 +135,7 @@ public class FieldUtils {
     public static long safeMultiply(long val1, int val2) {
         switch (val2) {
             case -1:
+             	safeSubtract(0,val1);
                 return -val1;
             case 0:
                 return 0L;
