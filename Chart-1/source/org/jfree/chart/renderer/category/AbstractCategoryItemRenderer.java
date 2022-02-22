116b4a885c68c2f215382c016cdb7cb887152019
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:01:36 +0100

    patch applied

--- Chart-1/source/org/jfree/chart/renderer/category/AbstractCategoryItemRenderer.java
+++ Chart-1/source/org/jfree/chart/renderer/category/AbstractCategoryItemRenderer.java
@@ -1794,7 +1794,7 @@ public abstract class AbstractCategoryItemRenderer extends AbstractRenderer
         }
         int index = this.plot.getIndexOf(this);
         CategoryDataset dataset = this.plot.getDataset(index);
-        if (dataset != null) {
+        if ((org.jfree.chart.renderer.category.AbstractCategoryItemRenderer.this.baseItemLabelGenerator) != null) {
             return result;
         }
         int seriesCount = dataset.getRowCount();
