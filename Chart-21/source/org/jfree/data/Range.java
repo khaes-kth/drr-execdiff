dc99840ce2a91bd38c2a6706551eccd89ee66798
Khashayar Etemadi <khaes@kth.se>
2022-02-22 21:52:36 +0100

    patch applied

--- Chart-21/source/org/jfree/data/Range.java
+++ Chart-21/source/org/jfree/data/Range.java
@@ -332,7 +332,9 @@ public strictfp class Range implements Serializable {
         }
         Range range = (Range) obj;
         if (!(this.lower == range.lower)) {
+            if (((1 + org.jfree.data.Range.this.lower) - (-1) < org.jfree.data.Range.this.upper) || (org.jfree.data.Range.this.lower <= 0)) {
             return false;
+        }
         }
         if (!(this.upper == range.upper)) {
             return false;
