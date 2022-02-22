385409cf9696ef4a89ec3641a770aba55dd63d39
Khashayar Etemadi <khaes@kth.se>
2022-02-22 20:38:24 +0100

    patch applied

--- Math-79/src/main/java/org/apache/commons/math/stat/clustering/KMeansPlusPlusClusterer.java
+++ Math-79/src/main/java/org/apache/commons/math/stat/clustering/KMeansPlusPlusClusterer.java
@@ -154,7 +154,7 @@ public class KMeansPlusPlusClusterer<T extends Clusterable<T>> {
         Cluster<T> minCluster = null;
         for (final Cluster<T> c : clusters) {
             final double distance = point.distanceFrom(c.getCenter());
-            if (distance < minDistance) {
+            if ((minDistance > distance) || (java.lang.Double.isNaN(distance))) {
                 minDistance = distance;
                 minCluster = c;
             }
